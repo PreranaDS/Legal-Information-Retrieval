8c7d0a40134b47816da9a9d9647b8d8a8d94e140
Prerana D Saligram <92503599+PreranaDS@users.noreply.github.com>
2022-12-04 16:50:59 +0530

    Update googleword2vec.py

--- googleword2vec.py
+++ googleword2vec.py
@@ -2,5 +2,5 @@ import gensim.downloader as api
 import gensim
 path = api.load("word2vec-google-news-300", return_path=True)
 print(path)
-wvgoogle=gensim.models.KeyedVectors.load_word2vec_format('C:/Users/JNCPG/gensim-data/word2vec-google-news-300/word2vec-google-news-300.gz', binary=True)
+wvgoogle=gensim.models.KeyedVectors.load_word2vec_format('path', binary=True)
 print(wvgoogle['cat'])
